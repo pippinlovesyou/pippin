3902bc6aab3ef4aaf09526bd1fae14f5fa1bac6c
Yohei Nakajima <me@yoheinakajima.com>
2025-01-26 17:59:39 +0000

    fix attempt 2

--- tests/test_onboarding.py
+++ tests/test_onboarding.py
@@ -1,3 +1,5 @@
+import subprocess
+import time
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
